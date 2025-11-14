2d37de608058815e673044a4832f2716d46233f5
Leticia Adriana dos Santos <leticiaadrianas@gmail.com>
2025-11-14 13:25:17 -0300

    ADD: advantages container at home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import Banner from '../components/banner'
+import Advantages from '../components/advantages'
 import HomeGrid from '../components/cardsGrid'
 import History from '../components/history'
 
@@ -6,6 +7,7 @@ function HomePage () {
     return (
         <>
             <Banner/>
+            <Advantages/>
             <HomeGrid/>
             <History/>
 
